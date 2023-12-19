f4dbd91a351a877d3cc3ccaa3f58584b859c18f2
macbook2013 <user@users-MacBook-Pro.local>
2023-12-19 21:12:47 +0700

    Write testDefaultEnv

--- tests/Feature/EnvironmentTest.php
+++ tests/Feature/EnvironmentTest.php
@@ -4,19 +4,21 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Env;
 use Tests\TestCase;
 
 class EnvironmentTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function testGetEnv()
     {
-        $response = $this->get('/');
+        $youtube = env('YOUTUBE');
 
-        $response->assertStatus(200);
+        self::assertEquals('Programmer Zaman Now', $youtube);
+    }
+
+    public function testDefaultEnv()
+    {
+        $author = env('AUTHOR', 'Ratino');
+        self::assertEquals('Ratino', $author);
     }
 }
