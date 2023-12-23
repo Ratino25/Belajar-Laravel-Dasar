ae1c76283f67287a2823d723ea1eeecd20100181
macbook2013 <user@192.168.1.12>
2023-12-23 08:42:32 +0700

    Unit Test AppEnvironment Test

---
+++ tests/Feature/AppEnvironmentTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+use Illuminate\Support\Facades;
+
+class AppEnvironmentTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testAppEnv()
+    {
+    //    var_dump(App::environment());
+        if(App::environmnet()){
+            self::assertTrue(true);
+        }
+    }
+}
