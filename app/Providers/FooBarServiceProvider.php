dbca107e78900b930e4102c77108102bf159aaa6
Ratino <ratinoray@gmail.com>
2025-03-24 22:51:44 +0700

    fooBarServiceProvider

--- app/Providers/FooBarServiceProvider.php
+++ app/Providers/FooBarServiceProvider.php
@@ -2,10 +2,17 @@
 
 namespace App\Providers;
 
+use App\Data\Bar;
+use App\Data\Foo;
+use App\Services\HelloService;
+use App\Services\HelloServiceIndonesia;
 use Illuminate\Support\ServiceProvider;
 
 class FooBarServiceProvider extends ServiceProvider
 {
+    public array $singletons = [
+        HelloService::class => HelloServiceIndonesia::class
+    ];
     /**
      * Register services.
      *
@@ -13,7 +20,12 @@ class FooBarServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(Foo::class, function ($app){
+            return new Foo();
+        });
+        $this->app->singleton(Bar::class, function ($app){
+            return new Bar($app->make(Foo::class));
+        });
     }
 
     /**
