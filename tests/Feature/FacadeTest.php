51fa7a1886d01f0569612ba7064663a85c9d2c31
Ratino <ratinoray@gmail.com>
2025-04-08 18:10:26 +0700

    FacadeTest Done

--- tests/Feature/FacadeTest.php
+++ tests/Feature/FacadeTest.php
@@ -17,4 +17,27 @@ class FacadeTest extends TestCase
         self::assertEquals($firstName1, $firstName2);
     }
 
+    public function testConfigDepedency()
+    {
+        $config = $this->app->make('config');
+        $firstName3 = $config->get('contoh.author.config');
+        $firstName1 = config('contoh.author.config');
+        $firstName2 = Config::get('contoh.author.config');
+
+        self::assertEquals($firstName1, $firstName2);
+        self::assertEquals($firstName1, $firstName3);
+    }
+
+    public function testFacadeMock()
+    {
+        Config::shouldReceive('get')
+            ->with('contoh.author.first')
+            ->andReturn('Ratino Keren');
+
+        $firstName = Config::get('contoh.author.first');
+
+        self::assertEquals('Ratino Keren', $firstName);
+    }
+
+
 }
