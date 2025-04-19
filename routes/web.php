4564c45ff9dea2fae410e9abcafa847c25eb3970
Ratino <ratinoray@gmail.com>
2025-04-19 23:22:48 +0700

    Route parameter & Named Route

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,38 @@ Route::get('hello-world', function(){
     return view('hello.world',['name' => 'Ratino']);
 });
 
+Route::get('/products/{id}', function($productId){
+    return "Product $productId";
+})->name('product.detail');
+
+Route::get('products/{product}/items/{item}', function($productId, $itemId){
+   return "Product $productId, Item $itemId";
+})->name('product.item.detail');
+
+Route::get('categories/{id}', function($categoryId){
+   return "Category $categoryId";
+})->where('id', '[0-9]+')->name('category.detail');
+
+Route::get('/users/{id?}', function($id = '404') {
+   return "User $id";
+})->name('user.detail');
+
+Route::get('/conflict/ratino', function(){
+    return "Conflict aprlia tri cahyani";
+});
+
+Route::get('/conflict/{name}', function($name){
+    return "Conflict $name";
+});
+
+Route::get('/produk/{id}', function($id){
+    $link = route('product.detail', ['id' => $id]);
+    return "Link $link";
+});
+
+Route::get('/produk-redirect/{id}', function($id){
+    return redirect()->route('product.detail', ['id' => $id]);
+});
+
+
+
--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -28,5 +28,50 @@ class RoutingTest extends TestCase
             ->assertSeeText('404 Not Found');
     }
 
+    public function testRouteParameter()
+    {
+        $this->get('/products/1')
+            ->assertSeeText('Product 1');
+
+        $this->get('/products/2')
+            ->assertSeeText('Product 2');
+
+        $this->get('/products/1/items/xxxx')
+            ->assertSeeText('Product 1, Item xxxx');
+
+        $this->get('/products/2/items/yyyy')
+            ->assertSeeText('Product 2, Item yyyy');
+
+
+    }
+
+    public function testRouteParameterRegex()
+    {
+        $this->get('/categories/100')
+            ->assertSeeText('Category 100');
+
+        $this->get('/categories/ratino')
+            ->assertSeeText('404 Not Found');
+    }
+
+    public function testRouteParameterOptional()
+    {
+        $this->get('users/ratino')->assertSeeText('User ratino');
+        $this->get('/users/')->assertSeeText('User 404');
+    }
+
+    public function testRouteConflict()
+    {
+        $this->get('conflict/atc')->assertSeeText('Conflict atc');
+        $this->get('conflict/ratino')->assertSeeText('Conflict aprlia tri cahyani');
+    }
+
+    public function testNamedRoute()
+    {
+        $this->get('/produk/12345')->assertSeeText('Link http://localhost/products/12345');
+        $this->get('/produk-redirect/12345')->assertSeeText('products/12345');
+
+    }
+
 
 }
