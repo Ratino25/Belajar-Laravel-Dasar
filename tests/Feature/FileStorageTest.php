0586bce21926abf6c813137041398751c2c4f946
Ratino <ratinoray@gmail.com>
2025-05-04 22:41:34 +0700

    File Storage

---
+++ tests/Feature/FileStorageTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class FileStorageTest extends TestCase
+{
+    public function testStorage()
+    {
+        $fileSystem = Storage::disk('local');
+        $fileSystem->put('file.txt', "Aprilia Tri Cahyani");
+
+        $content = $fileSystem->get('file.txt');
+
+        self::assertEquals('Aprilia Tri Cahyani', $content);
+    }
+
+    public function testPublic()
+    {
+        $fileSystem = Storage::disk('public');
+        $fileSystem->put('file.txt', "Aprilia Tri Cahyani");
+
+        $content = $fileSystem->get('file.txt');
+
+        self::assertEquals('Aprilia Tri Cahyani', $content);
+    }
+
+}
